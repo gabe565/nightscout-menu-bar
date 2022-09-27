35d26079a3f5606764d38488783769e447d013eb
Gabe Cook <gabe565@gmail.com>
2022-09-27 00:20:15 -0500

    :art:  Tweak indent

--- internal/assets/convert-images.sh
+++ internal/assets/convert-images.sh
@@ -9,18 +9,18 @@ DENSITY=144
 BASEHEIGHT="$(bc <<< "$HEIGHT - $PAD")"
 
 for SRC in "$@"; do (
-    DEST="$(basename "${SRC%.*}.png")"
+  DEST="$(basename "${SRC%.*}.png")"
 
-    inkscape "$SRC" \
-        --export-height="$BASEHEIGHT" \
-        --export-type=png \
-        --export-filename=- \
-    | convert - \
-        -strip \
-        -background transparent \
-        -gravity center \
-        -extent "${HEIGHT}x${HEIGHT}" \
-        -units PixelsPerInch \
-        -density "$DENSITY" \
-        "$DEST"
+  inkscape "$SRC" \
+    --export-height="$BASEHEIGHT" \
+    --export-type=png \
+    --export-filename=- \
+  | convert - \
+    -strip \
+    -background transparent \
+    -gravity center \
+    -extent "${HEIGHT}x${HEIGHT}" \
+    -units PixelsPerInch \
+    -density "$DENSITY" \
+    "$DEST"
 ) done
