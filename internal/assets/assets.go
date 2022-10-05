32dcb4f4aaffe8a7bc10ac5c054c18105ff29b13
Gabe Cook <gabe565@gmail.com>
2022-10-04 22:21:48 -0500

    :bento: Add Windows icons

--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -2,26 +2,26 @@ package assets
 
 import _ "embed"
 
-//go:generate ./convert-images.sh icon-menu-bar.svg
+//go:generate ./convert-icon.sh icon-menu-bar.svg icon-menu-bar.png
 //go:embed icon-menu-bar.png
 var IconMenuBar []byte
 
-//go:generate ./convert-images.sh calendar-solid.svg
+//go:generate ./convert-icon.sh calendar-solid.svg calendar-solid.png
 //go:embed calendar-solid.png
 var Calendar []byte
 
-//go:generate ./convert-images.sh rectangle-history-solid.svg
+//go:generate ./convert-icon.sh rectangle-history-solid.svg rectangle-history-solid.png
 //go:embed rectangle-history-solid.png
 var RectangleHistory []byte
 
-//go:generate ./convert-images.sh square-up-right-solid.svg
+//go:generate ./convert-icon.sh square-up-right-solid.svg square-up-right-solid.png
 //go:embed square-up-right-solid.png
 var SquareUpRight []byte
 
-//go:generate ./convert-images.sh gear-solid.svg
+//go:generate ./convert-icon.sh gear-solid.svg gear-solid.png
 //go:embed gear-solid.png
 var Gear []byte
 
-//go:generate ./convert-images.sh xmark-solid.svg
+//go:generate ./convert-icon.sh xmark-solid.svg xmark-solid.png
 //go:embed xmark-solid.png
 var Xmark []byte
---
+++ internal/assets/convert-icon.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+HEIGHT=32
+PAD=4
+DENSITY=144
+
+BASEHEIGHT="$(bc <<< "$HEIGHT - $PAD")"
+
+SRC="$1"
+DEST="$2"
+
+inkscape "$SRC" \
+  --export-height="$BASEHEIGHT" \
+  --export-type=png \
+  --export-filename=- \
+| convert - \
+  -strip \
+  -background transparent \
+  -gravity center \
+  -extent "${HEIGHT}x${HEIGHT}" \
+  -units PixelsPerInch \
+  -density "$DENSITY" \
+  "$DEST"
