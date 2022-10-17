aeb03443fa35359ab21349119e07b58dde4e4468
Gabe Cook <gabe565@gmail.com>
2022-10-17 17:34:43 -0500

    :truck: Move Viper init to main package

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/gabe565/nightscout-menu-bar/internal/config"
 	"github.com/gabe565/nightscout-menu-bar/internal/ticker"
 	"github.com/gabe565/nightscout-menu-bar/internal/tray"
 )
 
 func main() {
-	if err := config.InitViper(); err != nil {
+	if err := InitViper(); err != nil {
 		go func() {
 			tray.Error <- err
 		}()
