d06edb8ff2d07ed903f2adcb4d61106e21e85d1e
Gabe Cook <gabe565@gmail.com>
2022-10-17 23:35:31 -0500

    :white_check_mark: Add missing Reading Unmarshal test

--- internal/nightscout/reading_test.go
+++ internal/nightscout/reading_test.go
@@ -1,6 +1,7 @@
 package nightscout
 
 import (
+	"fmt"
 	"github.com/gabe565/nightscout-menu-bar/internal/config"
 	"github.com/spf13/viper"
 	"testing"
@@ -141,6 +142,103 @@ func TestReading_DisplayBg(t *testing.T) {
 			}
 			if got := r.DisplayBg(); got != tt.want {
 				t.Errorf("DisplayBg() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var normalReading = `{
+  "mean": 100,
+  "last": 100,
+  "mills": %d,
+  "sgvs": [
+    {
+      "_id": "a",
+      "mgdl": 100,
+      "mills": %d,
+      "device": "xDrip-DexcomG5",
+      "direction": "Flat",
+      "filtered": 0,
+      "unfiltered": 0,
+      "noise": 1,
+      "rssi": 100,
+      "type": "sgv",
+      "scaled": 100
+    }
+  ]
+}`
+
+var lowReading = `{
+  "sgvs": [
+    {
+      "_id": "a",
+      "mgdl": 39,
+      "mills": %d,
+      "device": "xDrip-DexcomG5",
+      "direction": "Flat",
+      "filtered": 0,
+      "unfiltered": 0,
+      "noise": 1,
+      "rssi": 100,
+      "type": "sgv",
+      "scaled": 39
+    }
+  ]
+}`
+
+func TestReading_UnmarshalJSON(t *testing.T) {
+	now := time.Now()
+
+	type fields struct {
+		Mean      int
+		Last      int
+		Mills     Mills
+		Index     int
+		FromMills Mills
+		ToMills   Mills
+		Sgvs      []SGV
+	}
+	type args struct {
+		bytes []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"simple",
+			fields{},
+			args{[]byte(fmt.Sprintf(normalReading, now.UnixMilli(), now.UnixMilli()))},
+			false,
+		},
+		{
+			"low",
+			fields{},
+			args{[]byte(fmt.Sprintf(lowReading, now.UnixMilli()))},
+			false,
+		},
+		{
+			"error",
+			fields{},
+			args{[]byte("{")},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Reading{
+				Mean:      tt.fields.Mean,
+				Last:      tt.fields.Last,
+				Mills:     tt.fields.Mills,
+				Index:     tt.fields.Index,
+				FromMills: tt.fields.FromMills,
+				ToMills:   tt.fields.ToMills,
+				Sgvs:      tt.fields.Sgvs,
+			}
+			if err := r.UnmarshalJSON(tt.args.bytes); (err != nil) != tt.wantErr {
+				t.Errorf("UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
