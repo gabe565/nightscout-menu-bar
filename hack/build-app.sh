6bcf6bd10124b7b09ef3965dd661c5dbf01b7653
Gabe Cook <gabe565@gmail.com>
2023-08-07 18:06:48 -0500

    :sparkles: (macos): Add universal binary support

--- hack/build-app.sh
+++ hack/build-app.sh
@@ -9,10 +9,15 @@ SCRIPT_DIR="$(dirname "$0")"
 DIST="$SCRIPT_DIR/../dist"
 ASSETS="$SCRIPT_DIR/../assets"
 
-rm -rf "$DIST"/*
+rm -rf "$DIST"
+mkdir -p "$DIST"
 
-echo Build "$BINARY_NAME"
-go build -ldflags='-w -s' -o "$DIST/$BINARY_NAME" "$(git rev-parse --show-toplevel)"
+export GOOS=darwin CGO_ENABLED=1
+for ARCH in amd64 arm64; do
+  echo Build "$BINARY_NAME-$ARCH"
+  GOARCH="$ARCH" go build -ldflags='-w -s' -o "$DIST/$BINARY_NAME-$ARCH" "$(git rev-parse --show-toplevel)"
+done
+lipo -create -output "$DIST/$BINARY_NAME" "$DIST/$BINARY_NAME-amd64" "$DIST/$BINARY_NAME-arm64"
 echo ...done
 
 echo Generate "$APP_NAME.app"
