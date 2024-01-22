96442be2119feb09ea01563cc86490110a48c025
Gabe Cook <gabe565@gmail.com>
2024-01-21 23:08:01 -0600

    chore(nightscout): Changed scaled value to string

--- internal/nightscout/sgv.go
+++ internal/nightscout/sgv.go
@@ -1,15 +1,17 @@
 package nightscout
 
+import "encoding/json"
+
 type SGV struct {
-	ID         string `json:"_id"`
-	Device     string `json:"device"`
-	Direction  string `json:"direction"`
-	Filtered   int    `json:"filtered"`
-	Mgdl       int    `json:"mgdl"`
-	Mills      Mills  `json:"mills"`
-	Noise      int    `json:"noise"`
-	Rssi       int    `json:"rssi"`
-	Scaled     int    `json:"scaled"`
-	Type       string `json:"type"`
-	Unfiltered int    `json:"unfiltered"`
+	ID         string      `json:"_id"`
+	Device     string      `json:"device"`
+	Direction  string      `json:"direction"`
+	Filtered   int         `json:"filtered"`
+	Mgdl       int         `json:"mgdl"`
+	Mills      Mills       `json:"mills"`
+	Noise      int         `json:"noise"`
+	Rssi       int         `json:"rssi"`
+	Scaled     json.Number `json:"scaled"`
+	Type       string      `json:"type"`
+	Unfiltered int         `json:"unfiltered"`
 }
