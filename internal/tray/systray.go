638dfe71731f30e1db48747c0aa7faca15e70293
Gabe Cook <gabe565@gmail.com>
2024-02-23 03:43:57 -0600

    chore(tray): Remove nested Goroutine

--- internal/tray/systray.go
+++ internal/tray/systray.go
@@ -40,78 +40,76 @@ func onReady() {
 	prefs := items.NewPreferences()
 	quitItem := items.NewQuit()
 
-	go func() {
-		for {
-			select {
-			case <-openNightscoutItem.ClickedCh:
-				go func() {
-					u, err := nightscout.BuildUrlWithToken()
-					if err != nil {
-						Error <- err
-						return
-					}
-					if err := open.Run(u.String()); err != nil {
-						Error <- err
-					}
-				}()
-			case <-prefs.Url.ClickedCh:
-				go func() {
-					if err := prefs.Url.Prompt(); err != nil {
-						Error <- err
-					}
-				}()
-			case <-prefs.Token.ClickedCh:
-				go func() {
-					if err := prefs.Token.Prompt(); err != nil {
-						Error <- err
-					}
-				}()
-			case <-prefs.Units.ClickedCh:
-				go func() {
-					if err := prefs.Units.Prompt(); err != nil {
-						Error <- err
-					}
-				}()
-			case <-ReloadConfig:
-				prefs.Url.UpdateTitle()
-				prefs.Token.UpdateTitle()
-				prefs.Units.UpdateTitle()
-			case <-prefs.StartOnLogin.ClickedCh:
-				if prefs.StartOnLogin.Checked() {
-					if err := autostart.Disable(); err != nil {
-						Error <- err
-					}
-					prefs.StartOnLogin.Uncheck()
-				} else {
-					if err := autostart.Enable(); err != nil {
-						Error <- err
-					}
-					prefs.StartOnLogin.Check()
+	for {
+		select {
+		case <-openNightscoutItem.ClickedCh:
+			go func() {
+				u, err := nightscout.BuildUrlWithToken()
+				if err != nil {
+					Error <- err
+					return
 				}
-			case <-quitItem.ClickedCh:
-				systray.Quit()
-			case properties := <-Update:
-				errorItem.Hide()
-
-				value := properties.String()
-				systray.SetTitle(value)
-				lastReadingItem.SetTitle(value)
-
-				for i, reading := range properties.Buckets {
-					if i < len(historyVals) {
-						historyVals[i].SetTitle(reading.String())
-					} else {
-						entry := historyItem.AddSubMenuItem(reading.String(), "")
-						entry.Disable()
-						historyVals = append(historyVals, entry)
-					}
+				if err := open.Run(u.String()); err != nil {
+					Error <- err
 				}
-			case err := <-Error:
-				errorItem.SetTitle(err.Error())
-				errorItem.Show()
+			}()
+		case <-prefs.Url.ClickedCh:
+			go func() {
+				if err := prefs.Url.Prompt(); err != nil {
+					Error <- err
+				}
+			}()
+		case <-prefs.Token.ClickedCh:
+			go func() {
+				if err := prefs.Token.Prompt(); err != nil {
+					Error <- err
+				}
+			}()
+		case <-prefs.Units.ClickedCh:
+			go func() {
+				if err := prefs.Units.Prompt(); err != nil {
+					Error <- err
+				}
+			}()
+		case <-ReloadConfig:
+			prefs.Url.UpdateTitle()
+			prefs.Token.UpdateTitle()
+			prefs.Units.UpdateTitle()
+		case <-prefs.StartOnLogin.ClickedCh:
+			if prefs.StartOnLogin.Checked() {
+				if err := autostart.Disable(); err != nil {
+					Error <- err
+				}
+				prefs.StartOnLogin.Uncheck()
+			} else {
+				if err := autostart.Enable(); err != nil {
+					Error <- err
+				}
+				prefs.StartOnLogin.Check()
 			}
+		case <-quitItem.ClickedCh:
+			systray.Quit()
+		case properties := <-Update:
+			errorItem.Hide()
+
+			value := properties.String()
+			systray.SetTitle(value)
+			lastReadingItem.SetTitle(value)
+
+			for i, reading := range properties.Buckets {
+				if i < len(historyVals) {
+					historyVals[i].SetTitle(reading.String())
+				} else {
+					entry := historyItem.AddSubMenuItem(reading.String(), "")
+					entry.Disable()
+					historyVals = append(historyVals, entry)
+				}
+			}
+		case err := <-Error:
+			errorItem.SetTitle(err.Error())
+			errorItem.Show()
 		}
-	}()
+	}
 }
 
 func onExit() {
