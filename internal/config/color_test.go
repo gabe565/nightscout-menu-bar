1c4d9162a126193fe650881fa1ade98ed7c31104
Gabe Cook <gabe565@gmail.com>
2024-08-17 21:48:51 -0500

    test(config): Fix failing tests

--- internal/config/color_test.go
+++ internal/config/color_test.go
@@ -1,8 +1,10 @@
 package config
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHexColor_MarshalText(t *testing.T) {
@@ -11,31 +13,26 @@ func TestHexColor_MarshalText(t *testing.T) {
 		HexColor HexColor
 	}
 	tests := []struct {
-		name     string
-		fields   fields
-		wantText []byte
-		wantErr  bool
+		name    string
+		fields  fields
+		want    []byte
+		wantErr require.ErrorAssertionFunc
 	}{
-		{"white", fields{HexColor{R: 0xFF, G: 0xFF, B: 0xFF}}, []byte("#fff"), false},
-		{"black", fields{HexColor{}}, []byte("#000"), false},
-		{"red", fields{HexColor{R: 0xFF}}, []byte("#f00"), false},
-		{"green", fields{HexColor{G: 0xFF}}, []byte("#0f0"), false},
-		{"blue", fields{HexColor{B: 0xFF}}, []byte("#00f"), false},
-		{"blue-gray", fields{HexColor{R: 0x60, G: 0x7D, B: 0x8B}}, []byte("#607d8b"), false},
-		{"increment", fields{HexColor{R: 1, G: 2, B: 3}}, []byte("#010203"), false},
+		{"white", fields{HexColor{R: 0xFF, G: 0xFF, B: 0xFF}}, []byte("#fff"), require.NoError},
+		{"black", fields{HexColor{}}, []byte("#000"), require.NoError},
+		{"red", fields{HexColor{R: 0xFF}}, []byte("#f00"), require.NoError},
+		{"green", fields{HexColor{G: 0xFF}}, []byte("#0f0"), require.NoError},
+		{"blue", fields{HexColor{B: 0xFF}}, []byte("#00f"), require.NoError},
+		{"blue-gray", fields{HexColor{R: 0x60, G: 0x7D, B: 0x8B}}, []byte("#607d8b"), require.NoError},
+		{"increment", fields{HexColor{R: 1, G: 2, B: 3}}, []byte("#010203"), require.NoError},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			h := tt.fields.HexColor
-			gotText, err := h.MarshalText()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("MarshalText() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotText, tt.wantText) {
-				t.Errorf("MarshalText() gotText = %v, want %v", string(gotText), string(tt.wantText))
-			}
+			got, err := h.MarshalText()
+			tt.wantErr(t, err)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
@@ -49,28 +46,24 @@ func TestHexColor_UnmarshalText(t *testing.T) {
 		name    string
 		args    args
 		want    HexColor
-		wantErr bool
+		wantErr require.ErrorAssertionFunc
 	}{
-		{"white", args{[]byte("#fff")}, HexColor{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xFF}, false},
-		{"black", args{[]byte("#000")}, HexColor{A: 0xFF}, false},
-		{"red", args{[]byte("#f00")}, HexColor{R: 0xFF, A: 0xFF}, false},
-		{"green", args{[]byte("#0f0")}, HexColor{G: 0xFF, A: 0xFF}, false},
-		{"blue", args{[]byte("#00f")}, HexColor{B: 0xFF, A: 0xFF}, false},
-		{"blue-gray", args{[]byte("#607d8b")}, HexColor{R: 0x60, G: 0x7D, B: 0x8B, A: 0xFF}, false},
-		{"missing-prefix", args{[]byte("fff")}, HexColor{}, true},
-		{"too-long", args{[]byte("#fffffff")}, HexColor{}, true},
-		{"too-short", args{[]byte("#fffff")}, HexColor{}, true},
+		{"white", args{[]byte("#fff")}, HexColor{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xFF}, require.NoError},
+		{"black", args{[]byte("#000")}, HexColor{A: 0xFF}, require.NoError},
+		{"red", args{[]byte("#f00")}, HexColor{R: 0xFF, A: 0xFF}, require.NoError},
+		{"green", args{[]byte("#0f0")}, HexColor{G: 0xFF, A: 0xFF}, require.NoError},
+		{"blue", args{[]byte("#00f")}, HexColor{B: 0xFF, A: 0xFF}, require.NoError},
+		{"blue-gray", args{[]byte("#607d8b")}, HexColor{R: 0x60, G: 0x7D, B: 0x8B, A: 0xFF}, require.NoError},
+		{"missing-prefix", args{[]byte("fff")}, HexColor{}, require.Error},
+		{"too-long", args{[]byte("#fffffff")}, HexColor{}, require.Error},
+		{"too-short", args{[]byte("#fffff")}, HexColor{}, require.Error},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			h := &HexColor{}
-			if err := h.UnmarshalText(tt.args.text); (err != nil) != tt.wantErr {
-				t.Errorf("UnmarshalText() error = %v, wantErr %v", err, tt.wantErr)
-			}
-			if !reflect.DeepEqual(h, tt.want) {
-				t.Errorf("MarshalText() gotText = %v, want %v", h, tt.want)
-			}
+			h := HexColor{}
+			tt.wantErr(t, h.UnmarshalText(tt.args.text))
+			assert.Equal(t, tt.want, h)
 		})
 	}
 }
